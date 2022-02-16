b7c54b34082251918ebbc3570e62b6076c3772b9
ricardo guadalupe <ricardoguadalupebetancor@gmail.com>
2022-02-16 18:30:56 +0000

    merge

--- js/script.js
+++ js/script.js
@@ -1,124 +1,41 @@
-var canciones;
-strCanciones = '[{"imgSrc":"media/images/1.jpg","songName":"Cantina Rag", "audioSrc":"media/songs/Jackson F. Smith - Cantina Rag.mp3", "artistName":"Jackson F.Smith"},{"imgSrc":"media/images/2.jpg","songName":"BuckBreak", "audioSrc":"media/songs/Ken Hamm - Buckbreak.mp3", "artistName":"Ken Hamm"},{"imgSrc":"media/images/3.jpg","songName":"Little Grass Shack", "audioSrc":"media/songs/Voodoo Suite - Little Grass Shack.mp3", "artistName":"Voodoo Suite"}]';
 
-canciones = JSON.parse(strCanciones);
-var videos;
-strVideos = '[{"imgSrc":"media/images/Ink.png","videoName":"Ink", "videoSrc":"media/videos/Ink - 67358.mp4", "artistName":"Pixabay"},{"imgSrc":"media/images/Road.png","videoName":"Road", "videoSrc":"media/videos/Road - 84970.mp4", "artistName":"Pixabay"},{"imgSrc":"media/images/Sea.png","videoName":"Sea", "videoSrc":"media/videos/Sea - 4006.mp4", "artistName":"Pixabay"}]';
-videos = JSON.parse(strVideos);
+// // Escuchar el elemento AUDIO
+// audio.addEventListener("timeupdate", updateProgress)
 
-// función IIFE para cargar cosas desde el inicio de la página, al entrar cargamos las canciones por defecto
-(function () {
-    var linkAudio = document.getElementById("linkAudio");
-    var linkVideo = document.getElementById("linkVideo");
-    linkAudio.addEventListener('click', cambioAudio);
-    linkVideo.addEventListener('click',cambioVideo);
-    
-    cambioAudio();
-})();
+// // Escuchar clicks en los controles
+// play.addEventListener("click", () => {
+//     if (audio.paused) {
+//         playSong()   
+//     } else {
+//         pauseSong()
+//     }
+// })
 
-function cambioAudio() {
-    vaciarUL();
-    var contenido = document.getElementById("contenido");
+// next.addEventListener("click", () => nextSong())
+// prev.addEventListener("click", () => prevSong())
+// //
 
-    canciones.forEach(function(cancion){
 
-        var nuevoLi;
-        var titulo;
-        var artista;
-        var imagen;
-        var divTexto;
-        nuevoLi = document.createElement("li");
-        titulo = document.createElement("h2");
-        artista = document.createElement("h4");
-        imagen = document.createElement("img");
-        divTexto = document.createElement("div");
+// //actualizar controles
+// function updateControls() {
+//     if (audio.paused) {
+//         play.classList.remove("fa-pause")
+//         play.classList.add("fa-play")
+//     } else {
+//         play.classList.add("fa-pause")
+//         play.classList.remove("fa-play")
+//     }
+// }
+// // Reproducir canción
+// function playSong() {
+//     if (actualSong !== null) {
+//         audio.play()
+//         updateControls()
+//     }
+// }
 
-        //ponemos los distintos nodos hijos
-        contenido.appendChild(nuevoLi);
-        nuevoLi.appendChild(imagen);
-        nuevoLi.appendChild(divTexto);
-        divTexto.appendChild(titulo);
-        divTexto.appendChild(artista);
-        
-        //añadimos el contenido de los apartados
-        titulo.append(cancion.songName);
-        artista.append(cancion.artistName);
-        imagen.setAttribute("src",cancion.imgSrc);
-
-        //añadimos el atributo name en el li para poder sacar el nombre de la canción que nos interese al darle click
-        nuevoLi.setAttribute("name", cancion.songName)
-
-        //y ponemos el event listener para poner la canción
-        nuevoLi.addEventListener('click',empezarReproduccionCancion);
-    });
-}
-
-function cambioVideo() {
-    vaciarUL();
-    var contenido = document.getElementById("contenido");
-
-    videos.forEach(function(video){
-
-        var nuevoLi;
-        var titulo;
-        var artista;
-        var imagen;
-        var divTexto;
-        nuevoLi = document.createElement("li");
-        titulo = document.createElement("h2");
-        artista = document.createElement("h4");
-        imagen = document.createElement("img");
-        divTexto = document.createElement("div");
-
-        //ponemos los distintos nodos hijos
-        contenido.appendChild(nuevoLi);
-        nuevoLi.appendChild(imagen);
-        nuevoLi.appendChild(divTexto);
-        divTexto.appendChild(titulo);
-        divTexto.appendChild(artista);
-        
-        //añadimos el contenido de los apartados
-        titulo.append(video.videoName);
-        artista.append(video.artistName);
-        imagen.setAttribute("src",video.imgSrc);
-
-    });
-}
-
-function vaciarUL() {
-    var contenido = document.getElementById("contenido");
-
-    var numHijos = contenido.childElementCount;
-
-    for (var iteraciones = 0; iteraciones < numHijos; iteraciones++){
-        contenido.lastChild.remove();
-    }
-}
-function empezarReproduccionCancion(event) {
-    //añadiríamos aquí dentro la función para poner la cancion
-    var parent = event.target.parentElement;
-
-    var encontradoLi = false;
-
-    //tenemos que encontrar el elemento Li para poder coger el atributo name, pero el target del evento no es el li sino sus hijos, así que vamos subiendo hasta encontrarlo:
-    while(!encontradoLi) {
-        if(parent.tagName == "LI") {
-            encontradoLi = true;
-        }
-        else {
-            parent = parent.parentElement;
-        }
-    }
-
-    var nombreCancion = parent.getAttribute("name");
-
-    var cancionJSON = canciones.filter(element => element.songName == nombreCancion);
-
-    var srcCancion = cancionJSON[0].audioSrc;
-
-    var tagAudio = document.getElementById("tagAudio")
-
-    tagAudio.setAttribute("src",srcCancion);
-
-    //canciones.filter(element => element.songName == nombreCancion);
-}
+// // Pausar canción
+// function pauseSong() {
+//     audio.pause()
+//     updateControls()
+// }
